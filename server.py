35c9896f99e96c056aaf1ad5e69c915c4a3b626e
LizaRoelofsen <60733836+LizaRoelofsen@users.noreply.github.com>
2022-01-17 17:42:42 +0100

    Adding sliders (for initialization)

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from mesa.visualization.ModularVisualization import ModularServer
+from mesa.visualization.UserParam import UserSettableParameter
 
 from model import SocialForce
 from SimpleContinuousModule import SimpleCanvas
@@ -13,12 +14,34 @@ def boid_draw(agent):
 boid_canvas = SimpleCanvas(boid_draw, 500, 500)
 
 wall = Wall(np.array([0, 30]), np.array([50, 0]))
+
 model_params = {
-    "population": 100,
+    "population": UserSettableParameter(
+        "slider",
+        "Population",
+        10,
+        10,
+        1000,
+        description="The initial population",
+    ),
     "width": 100,
     "height": 100,
-    "speed": 5,
-    "vision": 10,
+    "speed": UserSettableParameter(
+        "slider",
+        "Speed",
+        1,
+        1,
+        10,
+        description="Speed of Agent",
+    ),
+    "vision": UserSettableParameter(
+        "slider",
+        "Vision",
+        1,
+        1,
+        100,
+        description="Vision of the agents",
+    ),
     "obstacles": [wall],
     "dest": np.array([0, 0])
 }
