Fix wall, gradient and something else

human.py
@@ -47,13 +47,13 @@ class Human(Agent):
     def _gradient(func, val):
         """Compute the gradient of func at val using finite differencing"""
         # Settings for gradient descent
-        delta = 1e-3
+        delta = 1e-5
         dx = delta * np.array([1, 0])
         dy = delta * np.array([0, 1])
 
         # Compute the gradient per variable
-        grad_x = (func(val - dx) + func(val + dx)) / (2 * delta)
-        grad_y = (func(val - dy) + func(val + dy)) / (2 * delta)
+        grad_x = (func(val + dx) - func(val - dx)) / (2 * delta)
+        grad_y = (func(val + dy) - func(val - dy)) / (2 * delta)
 
         # Return the complete gradient
         return np.array([grad_x, grad_y])
@@ -90,7 +90,7 @@ class Human(Agent):
         def f(x): return v(b(x))
         return -1.0 * Human._gradient(f, r)
 
-    def obstacle_effect(self, obstacle_point):
+    def obstacle_effect(self, obstacle):
         """Repulsive effect from an obstacle"""
         def u(r):
             """Potential function"""
@@ -99,8 +99,12 @@ class Human(Agent):
             return u0 * np.exp(-np.linalg.norm(r)/R)
 
         # We do gradient descent, so return the negative gradient
-        r = self.pos - obstacle_point
-        return -1.0 * self._gradient(u, r)
+        def f(x):
+            obstacle_point = obstacle.get_closest_point(x)
+            r = self.pos - obstacle_point
+            return u(r)
+
+        return -1.0 * self._gradient(f, self.pos)
 
     def attract_effect(self, other_point):
         """Attractive effect to places/people of interest"""
@@ -133,15 +137,15 @@ class Human(Agent):
 
         # Compute repulsive effect from obstacles
         for obstacle in self.model.obstacles:
-            obstacle_point = obstacle.get_closest_point(self.pos)
-            self.velocity += self.obstacle_effect(obstacle_point)
+            self.velocity += self.obstacle_effect(obstacle)
 
         # Compute attractive effect to points/people of interest
         # TODO. Currently not implemented
 
         # Update the position
         self.speed = np.clip(np.linalg.norm(self.velocity), 0, self.max_speed)
-        self.velocity /= np.linalg.norm(self.velocity) * self.speed
+        self.velocity /= np.linalg.norm(self.velocity)
+        self.velocity *= self.speed
         new_pos = self.pos + self.velocity
 
         # if out of bounds, put at bound

server.py
@@ -13,7 +13,7 @@ def boid_draw(agent):
 
 boid_canvas = SimpleCanvas(boid_draw, 500, 500)
 
-wall = Wall(np.array([0, 30]), np.array([50, 0]))
+wall = Wall(np.array([50, 0]), np.array([50, 200]))
 
 model_params = {
     "population": UserSettableParameter(

wall.py
@@ -20,10 +20,10 @@ class Wall(Obstacle):
         """Compute the closest point of the obstacle to the given point"""
         line_vec = self.p2 - self.p1
         # Compute a normal of p2 - p1
-        v = np.flip(line_vec)
+        v = np.copy(np.flip(line_vec))
         v[0] *= -1.0
 
-        # Solve the system defined by p1 + a*(p2-1) = other_point + b*v)
+        # Solve the system defined by p1 + a*(p2-p1) = other_point + b*v)
         # TODO: check if this efficient, or can be done in a clearer way
         A = np.stack((line_vec, -v), axis=1)
         res = np.linalg.solve(A, other_point - self.p1)