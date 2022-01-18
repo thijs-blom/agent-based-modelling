32dfd8445458772f2f769923a2dccc51cb4ff762
Thijs Blom <matthijs.b.blom@gmail.com>
2022-01-18 17:23:10 +0100

    actually commit the simplecanvas code this time

--- SimpleContinuousModule.py
+++ SimpleContinuousModule.py
@@ -7,11 +7,12 @@ class SimpleCanvas(VisualizationElement):
     canvas_height = 500
     canvas_width = 500
 
-    def __init__(self, portrayal_method, canvas_height=500, canvas_width=500):
+    def __init__(self, agent_portrayal, wall_portrayal, canvas_height=500, canvas_width=500):
         """
         Instantiate a new SimpleCanvas
         """
-        self.portrayal_method = portrayal_method
+        self.agent_portrayal = agent_portrayal
+        self.wall_portrayal = wall_portrayal
         self.canvas_height = canvas_height
         self.canvas_width = canvas_width
         new_element = "new Simple_Continuous_Module({}, {})".format(
@@ -22,11 +23,18 @@ class SimpleCanvas(VisualizationElement):
     def render(self, model):
         space_state = []
         for obj in model.schedule.agents:
-            portrayal = self.portrayal_method(obj)
+            portrayal = self.agent_portrayal(obj)
             x, y = obj.pos
             x = (x - model.space.x_min) / (model.space.x_max - model.space.x_min)
             y = (y - model.space.y_min) / (model.space.y_max - model.space.y_min)
             portrayal["x"] = x
             portrayal["y"] = y
             space_state.append(portrayal)
+
+        for obj in model.obstacles:
+            portrayal = self.wall_portrayal(obj)
+            portrayal["pos1"] = obj.p1.tolist()
+            portrayal["pos2"] = obj.p2.tolist()
+            space_state.append(portrayal)
+
         return space_state
