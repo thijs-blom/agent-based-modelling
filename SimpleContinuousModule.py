12e8f2e450cd8e5ba4bd39f47dd3bc77f25c5274
LizaRoelofsen <60733836+LizaRoelofsen@users.noreply.github.com>
2022-01-25 10:22:58 +0100

    So you can chance the shape of the canvas (they mixed up the order)

--- SimpleContinuousModule.py
+++ SimpleContinuousModule.py
@@ -5,10 +5,10 @@ from dead import Dead
 class SimpleCanvas(VisualizationElement):
     local_includes = ["simple_continuous_canvas.js"]
     portrayal_method = None
-    canvas_height = 500
-    canvas_width = 500
+    #canvas_height = 500
+    #canvas_width = 500
 
-    def __init__(self, agent_portrayal, wall_portrayal, dead_portrayal, canvas_height=500, canvas_width=500):
+    def __init__(self, agent_portrayal, wall_portrayal, dead_portrayal, canvas_width, canvas_height):
         """
         Instantiate a new SimpleCanvas
         """
@@ -27,24 +27,31 @@ class SimpleCanvas(VisualizationElement):
         for obj in model.schedule.agents:
             portrayal = self.agent_portrayal(obj)
             x, y = obj.pos
-            x = (x - model.space.x_min) / (model.space.x_max - model.space.x_min)
-            y = (y - model.space.y_min) / (model.space.y_max - model.space.y_min)
-            portrayal["x"] = x
-            portrayal["y"] = y
+            x1 = x / model.space.x_max
+            y1 = y / model.space.y_max
+            portrayal["x"] = x1
+            portrayal["y"] = y1
             space_state.append(portrayal)
+
+            # portrayal = self.wall_portrayal(obj)
+            # portrayal["pos1"] = [x1*self.canvas_width, y1* self.canvas_height]
+            # portrayal["pos2"] = [0, 1]
+            # portrayal['w'] = 1
+            # space_state.append(portrayal)
+
 
         for obj in model.obstacles:
             if isinstance(obj, Dead):
                 portrayal = self.dead_portrayal(obj)
                 x, y = obj.pos
-                x = (x - model.space.x_min) / (model.space.x_max - model.space.x_min)
-                y = (y - model.space.y_min) / (model.space.y_max - model.space.y_min)
+                x /= model.space.x_max
+                y /= model.space.y_max
                 portrayal["x"] = x
                 portrayal["y"] = y
                 space_state.append(portrayal)
             else:
                 portrayal = self.wall_portrayal(obj)
-                    
+                
                 x1 = obj.p1[0] * (self.canvas_width / model.space.x_max)
                 y1 = obj.p1[1] * (self.canvas_height / model.space.y_max)
                 x2 = obj.p2[0] * (self.canvas_width / model.space.x_max)
