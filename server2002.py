cba663d2b5c529ba716d7348e6d1bf99f76bc9a8
LizaRoelofsen <60733836+LizaRoelofsen@users.noreply.github.com>
2022-01-25 10:25:00 +0100

    So its clear where to change the canvas

--- server2002.py
+++ server2002.py
@@ -18,7 +18,7 @@ def wall_draw(wall):
 def dead_draw(dead):
     return {"Shape": "circle", "r": 1, "Filled": "true", "Color": "Black"}
 
-canvas = SimpleCanvas(human_draw, wall_draw, dead_draw, 500, 500)
+canvas = SimpleCanvas(human_draw, wall_draw, dead_draw, canvas_width=500, canvas_height=500))
 chart0 =  ChartModule([{"Label": "Number of Humans in Environment", "Color": "#AA0000"}], 10, 25)
 chart1 =  ChartModule([{"Label": "Number of Casualties", "Color": "#AA0000"}], 10, 25)
 chart2 =  ChartModule([{"Label": "Average Energy", "Color": "#AA0000"}], 10, 25)
@@ -27,14 +27,14 @@ chart3 =  ChartModule([{"Label": "Average Speed", "Color": "#AA0000"}], 10, 25)
 width = 100
 height = 100
 
-wall1 = Wall(np.array([50, 0]), np.array([50, 200]))
+wall1 = Wall(np.array([50, 0]), np.array([50, 50]))
 side_wall1 = Wall(np.array([0, 2]), np.array([0, height]))
 side_wall2 = Wall(np.array([0, 0]), np.array([width, 0]))
 side_wall3 = Wall(np.array([width, 0]), np.array([width, height]))
 side_wall4 = Wall(np.array([0, height]), np.array([48, height]))
 side_wall5 = Wall(np.array([50, height]), np.array([width, height]))
 
-init_obstacles = [side_wall1, side_wall2, side_wall3, side_wall4, side_wall5]
+init_obstacles = [side_wall1, side_wall2, side_wall3, side_wall4, side_wall5, wall1]
 
 exit1 = Exit(np.array([0,0]), np.array([0,2]))
 exit2 = Exit(np.array([48,100]), np.array([50,100]))
