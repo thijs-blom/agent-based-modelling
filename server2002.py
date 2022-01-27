847a3fa101feccabbdd77ba34ef6a247c76c3655
LizaRoelofsen <60733836+LizaRoelofsen@users.noreply.github.com>
2022-01-27 17:03:08 +0100

    remove unnecessary

--- server2002.py
+++ server2002.py
@@ -2,7 +2,6 @@ from mesa.visualization.ModularVisualization import ModularServer
 from mesa.visualization.UserParam import UserSettableParameter
 from mesa.visualization.modules import ChartModule
 from mesa.visualization.ModularVisualization import VisualizationElement
-from mesa.batchrunner import BatchRunner
 
 from SimpleContinuousModule import SimpleCanvas
 from exit import Exit
@@ -105,52 +104,6 @@ model_params = {
 }
 
 # Define and launch the server
-#server = ModularServer(SocialForce, [canvas, chart0, chart2, chart3], "Escape Panic", model_params)
+server = ModularServer(SocialForce, [canvas, chart0, chart2, chart3], "Escape Panic", model_params)
 
-# # Set up all the parameters to be entered into the model
-# model_params = {
-#     "width": width,
-#     "height": height,
-#     "obstacles": init_obstacles,
-#     "exits": [exit1, exit2]
-# }
-
-# model_reporters = {
-#     "Number of Humans in Environment": lambda m: m.schedule.get_agent_count(),
-#     # "Number of Casualties": lambda m: len(self.obstacles) - self.init_amount_obstacles,
-#     # "Average Energy": lambda m: self.count_energy(m) / self.population,
-#     "Average Speed": lambda m: m.count_speed() / m.schedule.get_agent_count() if m.schedule.get_agent_count() > 0 else 0
-#     }
-
-# parameters = {
-#     'names': ['population', 'relaxation_time', 'vision'],
-#     'bounds': [[10, 1000], [0.5, 0.1], [1, 10]]
-# }
-
-# distinct_samples = 2
-# data = {}
-
-# for i, var in enumerate(parameters['names']):
-#     # Get the bounds for this variable and get <distinct_samples> samples within this space (uniform)
-#     samples = np.linspace(*parameters['bounds'][i], num=distinct_samples)
-    
-#     # Keep in mind that wolf_gain_from_food should be integers. You will have to change
-#     # your code to acommodate for this or sample in such a way that you only get integers.
-#     if var == 'population':
-#         samples = np.linspace(*parameters['bounds'][i], num=distinct_samples, dtype=int)
-    
-#     batch = BatchRunner(SocialForce,
-#                         max_steps=10,
-#                         iterations=1,
-#                         fixed_parameters=model_params,
-#                         variable_parameters={var: samples},
-#                         model_reporters= model_reporters,
-#                         display_progress=True)
-#     batch.run_all()
-
-#     data[var] = batch.get_model_vars_dataframe()
-# file = open("test.txt", "w")
-# file.write(data)
-# file.close()
-
-#server.launch()
+server.launch()
