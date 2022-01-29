5f504231d400586aa350dd96ecebb70c8fdaced2
LizaRoelofsen <60733836+LizaRoelofsen@users.noreply.github.com>
2022-01-29 16:36:07 +0100

    exit too

--- oneexit.py
+++ oneexit.py
@@ -5,19 +5,21 @@ import numpy as np
 
 
 class OneExit(SocialForce):
+    '''Social Force but then for different doorsizes.'''
     def __init__(
-            self,
-            population: int = 100,
-            width: float = 100,
-            height: float = 100,
-            max_speed: float = 5,
-            vision: float = 10,
-            relaxation_time: float = 1,
-            timestep: float = 0.01,
-            door_size: float = 2
+        self,
+        population: int = 200,
+        width: float = 100,
+        height: float = 100,
+        max_speed: float = 5,
+        vision: float = 1,
+        relaxation_time: float = 1,
+        timestep: float = 0.01,
+        init_desired_speed: float = 2.0,
+        door_size: float = 1,
     ):
         # Pass along
-        super().__init__(population, width, height, max_speed, vision, relaxation_time, [], [], timestep)
+        super().__init__(population, width, height, max_speed, vision, relaxation_time, [], [], timestep, init_desired_speed)
 
         # Define walls
         self.obstacles = [
